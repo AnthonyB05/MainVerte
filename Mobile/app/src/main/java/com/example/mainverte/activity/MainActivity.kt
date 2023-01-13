e00d1169cc0439359840e082894139d7d30db599
Solène <solene.cuffet@hotmail.fr>
2023-01-13 09:36:59 +0100

    Design Home

--- Mobile/app/src/main/java/com/example/mainverte/activity/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/mainverte/activity/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -14,6 +16,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_default,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.menuAbout -> {
+                Toast.makeText(this,"test", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,8 +50,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun verifInternet(){
         val positiveButtonClick = { dialog: DialogInterface, which: Int ->
-            Toast.makeText(applicationContext,
-                android.R.string.no, Toast.LENGTH_SHORT).show()
+
         }
         if (!Network.isNetworkAvailable(this)){
             val builder = AlertDialog.Builder(this)
@@ -50,11 +70,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun visibleRefreshButton(result: Boolean) {
         if (result){
-            buttonMaps.visibility =View.VISIBLE;
+            buttonMaps.visibility = View.VISIBLE;
+            buttonFav.visibility = View.VISIBLE;
+            buttonBalise.visibility = View.VISIBLE
+            buttonLocalisation.visibility = View.VISIBLE
             buttonRefresh.visibility = View.GONE
         }
         else{
-            buttonMaps.visibility =View.GONE;
+            buttonMaps.visibility = View.GONE;
+            buttonFav.visibility = View.GONE;
+            buttonBalise.visibility = View.GONE
+            buttonLocalisation.visibility = View.GONE
             buttonRefresh.visibility = View.VISIBLE
         }
     }
