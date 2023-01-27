f186b3d1a59cc44cbdca694c791f3b908e80b2bb
Hugo piai <piai.hugo@gmail.com>
2023-01-27 15:49:29 +0100

    fix long lat

--- api_test/model/balise-data.js
+++ api_test/model/balise-data.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
 const baliseData = new mongoose.Schema({
-    idBalise: Number,
-    degreCelsius:Number,
-    humiditeExt: Number,
-    luminosite: Number,
-    longitude: Number,
-    latitude: Number,
-    date: { type: Date, default: Date.now },
-  },{
-    versionKey: false,
+  idBalise: Number,
+  degreCelsius: Number,
+  humiditeExt: Number,
+  luminosite: Number,
 
-  });
+  date: { type: Date, default: Date.now },
+}, {
+  versionKey: false,
 
-  module.exports = mongoose.model('Data', baliseData,"Data")
+});
+
+module.exports = mongoose.model('Data', baliseData, "Data")
