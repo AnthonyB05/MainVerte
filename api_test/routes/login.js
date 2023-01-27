e3180b9344a3380d46c8c9f9fe04a574adab00ba
Hugo <piai.hugo@gmail.com>
2023-01-27 02:04:42 +0100

    add token acces api

---
+++ api_test/routes/login.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+let jwt = require('jsonwebtoken');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+/* Post users Login. */
+router.post('/', function (req, res, next) {
+    let userdata = {
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    //Go to server for user varificarion
+    if (userdata.username == "test" && userdata.password == "test") {
+        let token = jwt.sign(userdata, global.config.secretKey, {
+            algorithm: global.config.algorithm,
+            expiresIn: '7d'
+        });
+
+        res.status(200).json({
+            message: 'Login Successful',
+            jwtoken: token
+        });
+    }
+    else {
+        res.status(401).json({
+            message: 'Login Failed'
+        });
+    }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express()
 const balisesRoute = require("./api_test/routes/balises");
 const baliseDataRoute = require("./api_test/routes/balises-data");
+const login = require("./api_test/routes/login");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+global.config = require('./config');
 
 
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
+app.use('/login',login);
 app.use('/balises',balisesRoute);
 app.use('/balises-data',baliseDataRoute);
 app.use('/balise/:id',balisesRoute)
