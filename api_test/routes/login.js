55699b119367abe72c8b431676426db26965a635
Hugo piai <piai.hugo@gmail.com>
2023-01-27 15:56:39 +0100

    remove json

--- api_test/routes/login.js
+++ api_test/routes/login.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 let jwt = require('jsonwebtoken');
 
+
+// Use to generate tok
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+
     res.send('respond with a resource');
 });
 
